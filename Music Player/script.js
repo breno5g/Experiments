5ce631b38da16d28b9a42d871c6acc4c6a1aa90d
breno5g <brenosantos145@gmail.com>
2021-02-11 20:41:22 -0300

    Create the mute function

--- Music Player/script.js
+++ Music Player/script.js
@@ -1,7 +1,13 @@
 let audio = document.getElementById("audio")
+let progressBar = document.querySelector(".currentTimeBar");
+let currentTime = 0;
+let actualVol;
+let playInterval;
+
+audio.volume = 0.5;
 
 function playPause() {
-    let btn = event.target
+    let btn = event.target;
     if (audio.classList.contains("playing") == true) {
         audio.pause();
         audio.classList.remove("playing");
@@ -34,41 +40,36 @@ function turnDown() {
     }
 }
 
-let teste = document.querySelector(".currentTimeBar");
+/* =-=-=-=-= Mute buttom =-=-=-=-= */
+function mute() {
+    let btn = document.getElementById("vol")
+    if (btn.classList.contains("active")) {
+        btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-2"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>'
+        btn.classList.remove("active");
+        audio.volume = actualVol;
+    } else {
+        btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-x"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><line x1="23" y1="9" x2="17" y2="15"></line><line x1="17" y1="9" x2="23" y2="15"></line></svg>'
+        btn.classList.add("active");
+        actualVol = audio.volume;
+        audio.volume = 0;
+    }
+    // console.log(actualVol)
+}
 
-let valor = 0;
-
-let n;
+/* =-=-=-=-= Time bar =-=-=-=-= */
 function playBar() {
     if (audio.classList.contains("playing") == true) {
-        n = setInterval(() => {
-            teste.value = valor; 
-            valor++
-            console.log(teste.value)
-        }, 2120)
+        playInterval = setInterval(() => {
+            progressBar.value = currentTime; 
+            currentTime++
+            // console.log(progressBar.value)
+        }, (audio.duration / 100) * 1000)
     } else {
-        clearInterval(n)
+        clearInterval(playInterval)
     }
 }
 
 function setTime() {
-    audio.currentTime = teste.value * (audio.duration / 100);
-    valor = parseInt(teste.value);
-    console.log(teste.value)
-}
-
-// setInterval(() => {
-//     console.log(parseInt(audio.duration / 100))
-// }, 1500) 
-
-
-function change() {
-    let btn = document.getElementById("teste")
-    if (btn.classList.contains("active")) {
-        btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-2"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>'
-        btn.classList.remove("active");
-    } else {
-        btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-x"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><line x1="23" y1="9" x2="17" y2="15"></line><line x1="17" y1="9" x2="23" y2="15"></line></svg>'
-        btn.classList.add("active");
-    }
+    audio.currentTime = progressBar.value * (audio.duration / 100);
+    currentTime = parseInt(progressBar.value);
 }
