8a0c5bb68a86159e473201bc23fb7dfeee616e7e
breno5g <brenosantos145@gmail.com>
2021-02-11 13:29:40 -0300

    update

--- Music Player/script.js
+++ Music Player/script.js
@@ -60,3 +60,15 @@ function setTime() {
 // setInterval(() => {
 //     console.log(parseInt(audio.duration / 100))
 // }, 1500) 
+
+
+function change() {
+    let btn = document.getElementById("teste")
+    if (btn.classList.contains("active")) {
+        btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-2"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>'
+        btn.classList.remove("active");
+    } else {
+        btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-x"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><line x1="23" y1="9" x2="17" y2="15"></line><line x1="17" y1="9" x2="23" y2="15"></line></svg>'
+        btn.classList.add("active");
+    }
+}
