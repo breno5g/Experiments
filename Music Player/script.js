dcf173c5a64c5ea41f33f8e4c9af9cdf88093d19
breno5g <brenosantos145@gmail.com>
2021-02-12 09:37:21 -0300

    "finish" again

--- Music Player/script.js
+++ Music Player/script.js
@@ -92,9 +92,15 @@ function mute() {
 function changeVol() {
     let vol = document.getElementById("actualVol");
     let fill = document.getElementById("volFill");
+    let btn = document.getElementById("vol")
     fill.style = `width: ${volumeBar.value - 5}%;`;
     audio.volume = volumeBar.value * 0.01;
     vol.innerText = volumeBar.value;
+    if (audio.volume == 0) {
+        btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-x"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><line x1="23" y1="9" x2="17" y2="15"></line><line x1="17" y1="9" x2="23" y2="15"></line></svg>'
+    } else {
+        btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-volume-2"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>'
+    }
 }
 
 /* =-=-=-=-= Time bar =-=-=-=-= */
