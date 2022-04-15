1fcd08a77440dbe17f641cd0b3d8c1b9867bcefe
breno5g <brenosantos145@gmail.com>
2022-04-15 11:36:28 -0300

    melhorias no bot

--- webscraping/teste001/index.js
+++ webscraping/teste001/index.js
@@ -2,26 +2,83 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-const URL = 'https://mangahosted.com/manga/one-piece-br-mh80123/1046#1';
+const URL = 'https://mangahosted.com/manga/one-piece-br-mh80123';
+const folderNameArray = [];
 
-axios(URL).then(({ data }) => {
-  const html = data;
-  const $ = cheerio.load(html);
+const getChapters = async () => {
+  try {
+    const res = await axios.get(URL);
+    const { data: html } = res;
+    const $ = cheerio.load(html);
+    const chapters = [];
+    $('a.btn-green').each((index, n2) => {
+      if (index < 2) {
+        chapters.push($(n2).attr('href'));
+        folderNameArray.push($(n2).attr('title').split(' - ')[1]);
+      }
+    });
+    return chapters;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
-  const allPictures = $('picture');
-  const imgsArray = [];
-  allPictures.each(function () {
-    const img = $(this).find('img');
-    imgsArray.push(img.attr('src'));
+const getImagesLink = async () => {
+  const chaptersPage = await getChapters();
+  console.log(folderNameArray);
+
+  chaptersPage.forEach(async (cap, index) => {
+    const { data: html } = await axios.get(cap);
+    const $ = cheerio.load(html);
+    const allPictures = $('picture');
+    const imgsArray = [];
+    allPictures.each(function () {
+      const img = $(this).find('img');
+      imgsArray.push(img.attr('src'));
+    });
+    saveImages(imgsArray, index);
   });
-  fs.mkdirSync('./cap1046');
-  imgsArray.forEach((img, index) => {
+};
+
+const saveImages = (arr, fIndex) => {
+  fs.mkdirSync(folderNameArray[fIndex]);
+  arr.forEach((img, index) => {
     axios.get(img, { responseType: 'stream' }).then(({ data }) => {
       data.pipe(
         fs.createWriteStream(
-          `./cap1046/${(index + 1).toString().padStart(2, '0')}.jpg`
+          `./${folderNameArray[fIndex]}/${(index + 1)
+            .toString()
+            .padStart(2, '0')}.jpg`
         )
       );
     });
   });
-});
+};
+
+const init = () => {
+  getImagesLink();
+};
+
+init();
+
+// axios(URL).then(({ data }) => {
+//   const html = data;
+//   const $ = cheerio.load(html);
+
+// const allPictures = $('picture');
+// const imgsArray = [];
+// allPictures.each(function () {
+//   const img = $(this).find('img');
+//   imgsArray.push(img.attr('src'));
+// });
+//   fs.mkdirSync('./cap1046');
+//   imgsArray.forEach((img, index) => {
+//     axios.get(img, { responseType: 'stream' }).then(({ data }) => {
+//       data.pipe(
+//         fs.createWriteStream(
+//           `./cap1046/${(index + 1).toString().padStart(2, '0')}.jpg`
+//         )
+//       );
+//     });
+//   });
+// });
