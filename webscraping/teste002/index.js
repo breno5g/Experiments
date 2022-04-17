2249059818f9f8c7e44d5c76de6a532752303efa
breno5g <brenosantos145@gmail.com>
2022-04-16 21:42:42 -0300

    acho que tá funfando

--- webscraping/teste002/index.js
+++ webscraping/teste002/index.js
@@ -1,80 +1,77 @@
-const axios = require('axios');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const cheerio = require('cheerio');
+const axios = require('axios');
+require('dotenv').config();
 
-// (async () => {
-// const browser = await puppeteer.launch({ headless: false });
-// const page = await browser.newPage();
-// await page.goto('https://mangahosted.com/manga/one-piece-br-mh80123/1044#1');
-// const pageNumber = page.url().split('#')[1];
-// const imgURL = await page.$eval('#img_1', (img) => img.src);
-// console.log(imgURL);
-// await browser.close();
-// })();
+const URL =
+  'https://techwek.com/imagens-de-bom-dia-para-whatsapp-com-lindas-fotos-mensagens/';
+// 'https://www.google.com.br/search?q=imagens+de+bom+dia&hl=en&tbm=isch&source=hp&biw=1366&bih=624&ei=MDVbYtihG6q75OUP-MO88A4&iflsig=AHkkrS4AAAAAYltDQPpkX6-TiEGAkvqUJ6ASeA_Aabrt&ved=0ahUKEwjY-enCw5n3AhWqHbkGHfghD-4Q4dUDCAY&uact=5&oq=imagens+de+bom+dia&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgARQuwhYrBhg1RloAHAAeAGAAZ0CiAHOE5IBBjAuMTcuMZgBAKABAaoBC2d3cy13aXotaW1nsAEA&sclient=img';
 
-const URL = 'https://mangahosted.com/manga/one-piece-br-mh80123';
-const foldersNumber = [];
-
-const getChapters = async () => {
-  try {
-    const res = await axios.get(URL);
-    const { data: html } = res;
-    const $ = cheerio.load(html);
-    const chapters = [];
-    $('a.btn-green').each((index, n2) => {
-      const link = $(n2).attr('href');
-      const capName = link.split('/')[5];
-      if (Number(capName) >= 1 && Number(capName) <= 2) {
-        chapters.push(link);
-        foldersNumber.push(capName);
-      }
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(URL);
+  // await page.screenshot({ path: 'example.png' });
+  // Pegar os links das imagens e salvar
+  const imgsList = await page.evaluate(() => {
+    // Pegar as imagens
+    const imgsNodeList = document.querySelectorAll('.jetpack-lazy-image');
+    // converter para um array de imagens
+    const imgsArray = Array.from(imgsNodeList);
+    // pegar apenas o src e salvar em um objeto
+    return imgsArray.map(({ src }, index) => {
+      if (!src) return false;
+      return {
+        id: index + 1,
+        src,
+      };
     });
-    return chapters;
-  } catch (err) {
-    console.log(err.message);
-  }
-};
-
-const getLinks = async () => {
-  try {
-    const chaptersPage = await getChapters();
-
-    chaptersPage.forEach(async (cap, index) => {
-      const { data: html } = await axios.get(cap);
-      const $ = cheerio.load(html);
-      const allPictures = $('picture');
-      allPictures.each(async () => {
-        const img = $(this).find('img');
-        await buildArray(img.attr('src'), index);
-      });
-    });
-  } catch (err) {
-    console.log(err.message);
-  }
-  console.log(teste);
-};
-
-getLinks();
-
-const teste = [];
-const buildArray = async (src, index) => {
-  console.log(src);
-  const res = new Promise((resolve, reject) => {
-    if (!src) reject(new Error('deu ruim aqui, Cumpadi'));
-    teste.push({
-      link: src,
-      page: index,
-    });
-    resolve('tudo certo');
   });
+  // Salvar as imagens em um json caso eu queira criar uma função de não repetir imagens
+  // fs.writeFileSync('./imgsList.json', JSON.stringify(imgsList, null, 2));
 
-  return res;
+  // Selecionar uma imagem aleatoriamente
+  const randomNumber = Math.floor(Math.random() * imgsList.length - 1);
+  const selectedImage = imgsList[randomNumber].src;
+
+  // caso precise baixar a imagem
+  // await downloadImage(selectedImage);
+
+  await page.goto('http://discord.com/login');
+  await discordLogin(page);
+  await page.goto(
+    'https://discord.com/channels/943937409488392262/943937409488392265'
+  );
+  await sendImage(page, selectedImage);
+  await page.waitForTimeout(3000);
+
+  await browser.close();
+})();
+
+const downloadImage = async (src) => {
+  console.log(src);
+  const promise = new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.get(src, { responseType: 'stream' });
+      const { data } = res;
+      data.pipe(fs.createWriteStream('./bomDia.jpg'));
+      resolve('Tudo certo cumpadi');
+    } catch (err) {
+      reject(new Error('Download error: ' + err.message));
+    }
+  });
+  return promise;
 };
 
-// const init = async () => {
-//   const links = await getLinks();
-//   console.log(links);
-// };
+const discordLogin = async (page) => {
+  await page.type('input[name="email"]', process.env.EMAIL);
+  await page.type('input[name="password"]', process.env.PASSWORD);
+  page.click('button[type="submit"]');
+  await page.waitForNavigation({ waitUntil: 'networkidle2' });
+};
 
-// init();
+const sendImage = async (page, image) => {
+  await page.waitForTimeout(5000);
+  await page.keyboard.type(image);
+  await page.keyboard.press('Enter');
+};
